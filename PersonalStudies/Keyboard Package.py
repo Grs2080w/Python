8823f335041089c4c27be6ca8893476ead409479
Grs2080w <grs_santos2019@outlook.com>
2024-09-25 01:02:15 -0300

    Update Keyboard Package.py

--- PersonalStudies/Keyboard Package.py
+++ PersonalStudies/Keyboard Package.py
@@ -17,17 +17,17 @@ def ConAnd(f):
 #Tranforma uma frase em algo aplicável como parâmetro no kb.is_pressed do While abaixo (Apagar último 'and') (Recomendável Ctrl C Ctrl V).
 
 
-t = str(input('Frase:'))
-ConAnd(t)
+# t = str(input('Frase:'))
+# ConAnd(t)
 
 
-'''while True:
+while True:
     if kb.is_pressed('g' and 'a' and 'b' and 'r' and 'i' and 'e' and 'l'):
         p = pyautogui.position()
         pyautogui.click(p)
-        kb.write('Gabriel é demais')
-        print('Funçao cumprida')
-        break'''
+        kb.write('Messi is cool')
+        print('Function Done')
+        break
 
 
 ''' Na função acima é possível usando os comandos explicados, é criado um loop, e ao ser digitada
@@ -35,7 +35,7 @@ preditas, é lida a posição do Mouse, click nessa posição, escrito o texto p
 uma resposta e por fim um break  
 
 Detalhe: Pode-se usar uma palavra ou frase parâmetro pro kb.is_pressed, mas é necessário escrever
-da forma acima.
+da forma acima. (Portuguese)
 
 '''
 
